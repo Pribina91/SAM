3ce93ab2b7e42cafe0f6f141d4e087a4ea87567f
pribina91 <adostajo@gmail.com>
2014-11-02 20:43:41 +0100

    init commit - project creation

---
+++ SAM/VisualAnalytics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualAnalytics.Startup))]
+namespace VisualAnalytics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
